b20ed6dfa10f7310277507612b86200a4b409967
Valentin LECERF <ioxFR@users.noreply.github.com>
2015-08-23 14:20:56 +0000

    Initial Commit

---
+++ Main/Vlecerf.Com.SPCTLC/Vlecerf.Com.SPCTLC/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vlecerf.Com.SPCTLC")]
+[assembly: AssemblyDescription("SharePoint Content Type Lookup Column")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("vlecerf.com")]
+[assembly: AssemblyProduct("Vlecerf.Com.SPCTLC")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("valentin lecerf")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45520b54-730d-4c72-bacf-2d372aeb313b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
