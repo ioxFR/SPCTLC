f37a0dab1615e179334e2fb8cf257ff95885bc60
Valentin LECERF <ioxFR@users.noreply.github.com>
2015-08-24 17:59:23 +0000

    Code Comments

--- Main/SP2013/Vlecerf.Com.SPCTLC/Vlecerf.Com.SPCTLC/Fields/SPContentTypeLookupControl.cs
+++ Main/SP2013/Vlecerf.Com.SPCTLC/Vlecerf.Com.SPCTLC/Fields/SPContentTypeLookupControl.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.SharePoint;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// file:	Fields\SPContentTypeLookupControl.cs
+//
+// summary:	Implements the sp content type lookup control class
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
 using System;
 using System.Collections.Generic;
@@ -9,9 +15,22 @@ using System.Web.UI.WebControls;
 
 namespace Vlecerf.Com.SPCTLC.Fields
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   A sp content type lookup control. </summary>
+    ///
+    /// <remarks>   Administrator, 24/08/2015. </remarks>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     class SPContentTypeLookupControl : BaseFieldControl
     {
+        /// <summary>   Type of the ddl content. </summary>
         DropDownList ddlContentType;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets the name of the default rendering template. </summary>
+        ///
+        /// <value> A <see cref="T:System.String" /> object that names a rendering template. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         protected override string DefaultTemplateName
         {
@@ -20,6 +39,15 @@ namespace Vlecerf.Com.SPCTLC.Fields
                 return "ddlContentTypeRenderingTemplate";
             }
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Creates any child controls necessary to render the field, such as a label control, link
+        /// control, or text box control.
+        /// </summary>
+        ///
+        /// <remarks>   Administrator, 24/08/2015. </remarks>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         protected override void CreateChildControls()
         {
@@ -35,6 +63,18 @@ namespace Vlecerf.Com.SPCTLC.Fields
                 }
             }
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// When overridden in a derived class, gets or sets the value of the field in the UI.
+        /// </summary>
+        ///
+        /// <value>
+        /// When overridden in a derived class, a <see cref="T:System.Object" /> that represents the
+        /// value of the field in the UI. Typically, this is the text of a child control, such as a label
+        /// or text box, that actually renders the field.
+        /// </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         public override object Value
         {
--- Main/SP2013/Vlecerf.Com.SPCTLC/Vlecerf.Com.SPCTLC/Fields/SPContentTypeLookupField.cs
+++ Main/SP2013/Vlecerf.Com.SPCTLC/Vlecerf.Com.SPCTLC/Fields/SPContentTypeLookupField.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.SharePoint;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// file:	Fields\SPContentTypeLookupField.cs
+//
+// summary:	Implements the sp content type lookup field class
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
 using System;
 using System.Collections.Generic;
@@ -8,15 +14,51 @@ using System.Threading.Tasks;
 
 namespace Vlecerf.Com.SPCTLC.Fields
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   A sp content type lookup field. </summary>
+    ///
+    /// <remarks>   Administrator, 24/08/2015. </remarks>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     class SPContentTypeLookupField : SPFieldChoice
     {
         #region Constructors
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Administrator, 24/08/2015. </remarks>
+        ///
+        /// <param name="fields">       The fields. </param>
+        /// <param name="fieldName">    Name of the field. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public SPContentTypeLookupField(SPFieldCollection fields, string fieldName) : base(fields, fieldName) { }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Administrator, 24/08/2015. </remarks>
+        ///
+        /// <param name="fields">       The fields. </param>
+        /// <param name="typeName">     Name of the type. </param>
+        /// <param name="displayName">  Name of the display. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         public SPContentTypeLookupField(SPFieldCollection fields, string typeName, string displayName) : base(fields, typeName, displayName) { }
         #endregion
 
         #region Properties
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets the field type control that is used to render the field. </summary>
+        ///
+        /// <value>
+        /// A <see cref="T:Microsoft.SharePoint.WebControls.BaseFieldControl" /> object that represents
+        /// the control.
+        /// </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public override BaseFieldControl FieldRenderingControl
         {
             get
